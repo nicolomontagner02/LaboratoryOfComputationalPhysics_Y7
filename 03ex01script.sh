ef1a40391ab033e4057fe2d0474dc5682599b3c2
nicolomontagner02 <nicolo.montagner@studenti.unipd.it>
2024-11-20 11:09:11 +0100

    20-11-24

--- 03ex01script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir students
-wget -O studenti.csv https://www.dropbox.com/scl/fi/bxv17nrbrl83vw6qrkiu9/LCP_22-23_students.csv?rlkey=47fakvatrtif3q3qw4q97p5b7&e=1
-mv studenti.csv students/studenti.csv
-cd students
-touch studentiPoD.csv
-touch studentiOthers.csv
-grep -v "*PoD" studenti.csv > studentiPoD.csv
