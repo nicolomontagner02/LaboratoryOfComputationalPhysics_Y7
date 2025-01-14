a7e1f2d23918cf0e2d3db51b19360f5189a7a522
nicolomontagner02 <nicolo.montagner@studenti.unipd.it>
2025-01-14 21:38:36 +0100

    studying

---
+++ 03ex/03_2_script.sh
@@ -0,0 +1,34 @@
+grep -v "#" data.csv | sed "s/,//g" > data.txt
+
+n=$(grep -o "[0-9]\+" data.txt| wc -w)
+count=0
+
+c=$(grep -o "[0-9]\+" data.txt)
+
+for i in $c;
+do
+    f=$(echo $(($i%2)))
+    if [ $f -ne 0 ];
+    then
+        count=$(echo $(($count+1)))
+    fi
+done
+
+echo "There are $count even number in data.txt"
+
+l=$(grep "[0-9]\+" data.txt)
+
+for i in $l;
+do
+    if [ $count -le 2 ];
+    then
+        s=$(echo "scale=0;sqrt($j)" | bc)
+        s1=$(echo $(($s+$s1)))
+        count=$(echo $(($count+1)))
+    else
+        s=$(echo "scale=0;sqrt($j)" | bc)
+        s2=$(echo $(($s+$s2)))
+    fi
+    echo "$s1 and $s2"
+done
+
